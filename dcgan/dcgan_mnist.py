336a1c5ce8decc2db32da20a95aaa6b04ca4b109
Sevak Mardirosian <smardirosians@gmail.com>
2018-09-22 13:33:38 +0200

    exper with dcgan_mnist

---
+++ dcgan/dcgan_mnist.py
@@ -0,0 +1,10 @@
+import numpy as np
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
